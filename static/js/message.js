e9cc219c5c54f46c8dba8501bb1d2ffe79aa44ee
thanh-sys <phamthanhjob1@gmail.com>
2026-09-14 09:02:14 +0700

    old code

---
+++ static/js/message.js
@@ -0,0 +1,7 @@
+
+export const MESSAGES = {
+  NAME_ERROR: "Name should only have characters",
+  REQUIRED_SPORT: "Please choose at least 1 sport",
+  SPORT_DUPLICATE: "This sport already exists.",
+  ROLE_DUPLICATE: "This role already exists."
+};
